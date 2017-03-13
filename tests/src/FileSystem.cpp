c711a8bf14c307c24e187327800d27700d581965
Jarrett Chisholm <contact@jarrettchisholm.com>
2017-03-12 20:05:51 -0400

    added some tests

--- tests/src/FileSystem.cpp
+++ tests/src/FileSystem.cpp
@@ -3,27 +3,65 @@
 
 #include "fs/FileSystem.hpp"
 
-BOOST_AUTO_TEST_SUITE(FileSystem)
+struct Fixture
+{
+	Fixture()
+	{
+		fileSystem = hercules::fs::FileSystem();
+	}
+	
+	hercules::fs::FileSystem fileSystem;
+};
+
+BOOST_FIXTURE_TEST_SUITE(FileSystem, Fixture)
 
 BOOST_AUTO_TEST_CASE(constructor)
 {
-	auto fileSystem = hercules::fs::FileSystem();
 }
 
 BOOST_AUTO_TEST_CASE(doesNotExist)
 {
-	auto fileSystem = hercules::fs::FileSystem();
 	bool exists = fileSystem.exists( std::string("./DOES_NOT_EXIST") );
 
 	BOOST_CHECK_EQUAL(exists, false);
 }
 
-BOOST_AUTO_TEST_CASE(doesExist)
+BOOST_AUTO_TEST_CASE(doesFileExist)
 {
-	auto fileSystem = hercules::fs::FileSystem();
 	bool exists = fileSystem.exists( std::string("./fixtures/filesystem_test_file.txt") );
 
 	BOOST_CHECK_EQUAL(exists, true);
+}
+
+BOOST_AUTO_TEST_CASE(doesFolderExist)
+{
+	bool exists = fileSystem.exists( std::string("./fixtures") );
+
+	BOOST_CHECK_EQUAL(exists, true);
+}
+
+BOOST_AUTO_TEST_CASE(isNotDirectory)
+{
+	bool isDirectory = fileSystem.isDirectory( std::string("./fixtures/filesystem_test_file.txt") );
+
+	BOOST_CHECK_EQUAL(isDirectory, false);
+}
+
+BOOST_AUTO_TEST_CASE(isDirectory)
+{
+	bool isDirectory = fileSystem.isDirectory( std::string("./fixtures") );
+
+	BOOST_CHECK_EQUAL(isDirectory, true);
+}
+
+BOOST_AUTO_TEST_CASE(deleteFile)
+{
+	//fileSystem.createFile( std::string("./fixtures/delete_me.txt") );
+	//fileSystem.deleteFile( std::string("./fixtures/delete_me.txt") );
+	
+	//bool exists = fileSystem.exists( std::string("./fixtures/delete_me.txt") );
+
+	//BOOST_CHECK_EQUAL(exists, false);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
