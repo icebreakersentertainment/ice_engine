10ce678323f93e8f08b93588d045344d7e36cb53
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-08 17:29:14 +0000

    Adding tarfile member sanitization to extractall()

--- setup.py
+++ setup.py
@@ -207,7 +207,29 @@ def extract():
 
 		if not isWindows:
 			with tarfile.TarFile.open(filename, 'r:gz') as f:
-				f.extractall(path=dependenciesDirectory)
+	
+	import os
+	
+	def is_within_directory(directory, target):
+		
+		abs_directory = os.path.abspath(directory)
+		abs_target = os.path.abspath(target)
+	
+		prefix = os.path.commonprefix([abs_directory, abs_target])
+		
+		return prefix == abs_directory
+	
+	def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+	
+		for member in tar.getmembers():
+			member_path = os.path.join(path, member.name)
+			if not is_within_directory(path, member_path):
+				raise Exception("Attempted Path Traversal in Tar File")
+	
+		tar.extractall(path, members, numeric_owner=numeric_owner) 
+		
+	
+	safe_extract(f, path=dependenciesDirectory)
 				folders = [f for f in glob.glob(dependenciesDirectory + k + '*') if os.path.isdir(f)]
 				for f in folders:
 					os.rename(f, dependenciesDirectory+k)
